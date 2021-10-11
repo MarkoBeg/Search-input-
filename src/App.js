6fc4d6391b0f11ee7a554f47aaec645fe5ac26f5
Marko <markobeg15@gmail.com>
2021-10-11 15:32:18 +0200

    search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { data } from "./data";
+
 
 function App() {
+  const [filter, setFilter] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Search by Name or LastName</h1>
+      <input
+        type="text"
+        placeholder="Search"
+        onChange={(e) => setFilter(e.target.value)}
+      />
+      {data
+        .filter((value) => {
+          if (setFilter === "") {
+            return value;
+          } else if (
+            value.name.toLowerCase().includes(filter.toLowerCase()) ||
+            value.lastName.toLowerCase().includes(filter.toLowerCase())
+          ) {
+            return value;
+          }
+        })
+        .map((item, index) => {
+          return (
+            <div key={index}>
+              <h3>{item.name}</h3>
+              <h4>{item.lastName}</h4>
+            </div>
+          );
+        })}
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,75 @@
+const data = [
+  {
+    id: 1,
+    name: "Car",
+    lastName: "Car",
+  },
+  {
+    id: 2,
+    name: "Bobo",
+    lastName: "Ban",
+  },
+  {
+    id: 3,
+    name: "Milan",
+    lastName: "Mili",
+  },
+  {
+    id: 4,
+    name: "Jopa",
+    lastName: "Jozic",
+  },
+  {
+    id: 5,
+    name: "Ante",
+    lastName: "Antic",
+  },
+  {
+    id: 6,
+    name: "Karlo",
+    lastName: "Kralj",
+  },
+  {
+    id: 7,
+    name: "Ivan",
+    lastName: "Ivic",
+  },
+
+  {
+    id: 8,
+    name: "Marko",
+    lastName: "Beg",
+  },
+  {
+    id: 9,
+    name: "Borna",
+    lastName: "Busic",
+  },
+  {
+    id: 10,
+    name: "Elvis",
+    lastName: "Nunic",
+  },
+  {
+    id: 11,
+    name: "Roko",
+    lastName: "Podravec",
+  },
+  {
+    id: 12,
+    name: "Mike",
+    lastName: "Iron",
+  },
+  {
+    id: 13,
+    name: "Vladimir",
+    lastName: "Vander",
+  },
+  {
+    id: 14,
+    name: "John",
+    lastName: "White",
+  },
+];
+
+export { data };
